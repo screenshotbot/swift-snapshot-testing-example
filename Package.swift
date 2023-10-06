0bea3237f7f050303b2b17941737dd6cbc6fa3ab
Arnold Noronha <arnold@screenshotbot.io>
2023-10-06 19:12:29 -0400

    ..

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "test-app",
     platforms: [
         .iOS(.v15),
-        .macOS(.v14),
+        .macOS(.v13),
         .tvOS(.v13),
         .watchOS(.v6),
     ],
