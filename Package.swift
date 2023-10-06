e9ed210dc9d9cd8619422db2ac043e32583f0d64
Arnold Noronha <arnold@screenshotbot.io>
2023-10-06 18:31:12 -0400

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "test-app",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v14),
+        .tvOS(.v13),
+        .watchOS(.v6),
+    ],
+    products: [
+        .library(
+            name: "TestApp",
+            targets: ["TestApp"]
+        )
+    ],
+    dependencies: [
+      .package(
+        url: "https://github.com/pointfreeco/swift-snapshot-testing",
+        from: "1.12.0"
+      ),
+    ],
+    targets: [
+        .target(
+            name: "TestApp",
+            dependencies: [
+            ],
+            path: "TestApp"
+        ),
+        .testTarget(
+            name: "TestAppTests",
+            dependencies: [
+                "TestApp"
+            ],
+            path: "TestAppTests"
+        ),
+    ]
+)
